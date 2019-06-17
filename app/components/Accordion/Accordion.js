cad5ec0ec25995fc0212b14f2ca0ee17ff911066
Nicolas <devnicolasmozo@gmail.com>
2019-06-17 22:07:09 +0200

    Add simulated request AJAX

--- app/components/Accordion/Accordion.js
+++ app/components/Accordion/Accordion.js
@@ -1,11 +1,33 @@
 import './Accordion.scss'
 import AccordionSection from '../AccordionSection/AccordionSection'
+import AccordionItem from '../../models/AccordionItem'
 import Component from '../Component'
+import NewSection from '../../mocks/section.json'
 
 export default class Accordion extends Component {
+  async fetchAPI() {
+    const newItem = AccordionItem(NewSection.title, NewSection.content)
+
+    setTimeout(() => {
+      super.update(
+        this.ref,
+        {
+          ...this.props,
+          elements: [...this.props.elements, newItem],
+          updated: true
+        },
+        this
+      )
+    }, 4000)
+  }
+
   render() {
-    const { elements } = this.props
+    const { elements, updated } = this.props
     const dlElement = document.createElement('dl')
+
+    if (!updated) {
+      this.fetchAPI()
+    }
 
     if (!Array.isArray(elements)) {
       throw Error('You must pass an array of elements')
